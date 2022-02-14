344d6e3563efb61dd06ed80480804bd4e7204f96
AmanVernekar <amansuraj.av@gmail.com>
2022-02-14 15:07:34 +0000

    completed Task1B

---
+++ Task1B.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
+
+def run():
+    """Requirements for Task 1B"""
+    
+    p = (52.2053, 0.1218)
+    
+    # Build list of stations
+    stations = build_station_list()
+
+    # Get distances of stations from the given coordinate
+    distances = stations_by_distance(stations, p)
+
+    print("The 10 closest stations to Cambridge city centre are:")
+    print(distances[:10] + "\n")
+
+    print("The 10 stations furthest away from Cambridge city centre are:")
+    print(distances[-10:])
+
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***\n")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,27 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from math import asin, cos, sin, sqrt, pow
+
+""" This function takes a list of monitoring stations and a coordinate p, and returns a sorted
+list of (station, distance) tuples where distance is the distance of the station from p.
+"""
+def stations_by_distance(stations, p):
+    distances = []
+    for station in stations:
+        distance = haversine(station.coordinate, p)
+        distances.append((station, distance))
+    distances = sorted_by_key(distances)
+    return distances
+
+""" A function to calculate the haversine distance between two coordinates in kilometres.
+"""
+def haversine(c1, c2):
+    r = 6371
+    lat1, long1 = c1
+    lat2, long2 = c2
+
+    part1 = pow(sin((lat2 - lat1)/2), 2)
+    part2 = cos(long1) * cos(long2) * pow(sin((long2 - long1)/2), 2)
+    
+    distance = 2 * r * asin(sqrt(part1 + part2))

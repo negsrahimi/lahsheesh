b71a2f6eec852df4ad59032d4e4d4150f57d47d1
AmanVernekar <amansuraj.av@gmail.com>
2022-02-16 14:14:31 +0000

    Update geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,7 +7,8 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
-from math import asin, cos, sin, sqrt, pow
+#from math import asin, cos, sin, sqrt, pow
+from haversine import haversine
 
 """ This function takes a list of monitoring stations and a coordinate p, and returns a sorted
 list of (station, distance) tuples where distance is the distance of the station from p.
@@ -15,22 +16,24 @@ list of (station, distance) tuples where distance is the distance of the station
 def stations_by_distance(stations, p):
     distances = []
     for station in stations:
-        distance = haversine(station.coordinate, p)
+        distance = haversine(station.coord, p)
         distances.append((station, distance))
-    distances = sorted_by_key(distances)
+    distances = sorted_by_key(distances, 1)
     return distances
 
 """ A function to calculate the haversine distance between two coordinates in kilometres.
 """
-def haversine(c1, c2):
-    r = 6371
-    lat1, long1 = c1
-    lat2, long2 = c2
-
-    part1 = pow(sin((lat2 - lat1)/2), 2)
-    part2 = cos(long1) * cos(long2) * pow(sin((long2 - long1)/2), 2)
-    
-    distance = 2 * r * asin(sqrt(part1 + part2))
+##def haversine(c1, c2):
+##    r = 6371
+##    lat1, long1 = c1
+##    lat2, long2 = c2
+##
+##    part1 = pow(sin((lat2 - lat1)/2), 2)
+##    print(part1)
+##    part2 = cos(long1) * cos(long2) * pow(sin((long2 - long1)/2), 2)
+##    print(part2)
+##    
+##    distance = 2 * r * asin(sqrt(abs(part1 + part2)))
 
 """ Given a list of station objects, this function returns a list of rivers with a monitoring
 station.

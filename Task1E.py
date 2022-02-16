659cd687a49e1e12bc75a93a6423e45d7bbf53a5
Negar Rahimi <negsrahimi@gmail.com>
2022-02-16 13:51:04 +0000

    Task 1C and 1E final

--- Task1E.py
+++ Task1E.py
@@ -6,20 +6,14 @@ from floodsystem.stationdata import build_station_list
 
 
 def run():
-    """Requirements for Task 1A"""
+    """Requirements for Task 1E"""
 
     # Build list of stations
     stations = build_station_list()
 
-    # Print number of stations
-    print("Number of stations: {}".format(len(stations)))
-
-    # Display data from 3 stations:
-    for station in stations:
-        if station.name in [
-                'Bourton Dickler', 'Surfleet Sluice', 'Gaw Bridge'
-        ]:
-            print(station)
+    # Prints rivers with the value N=9
+    stations_no = rivers_by_station_number(stations, 9)
+    print(stations_no)
 
 
 if __name__ == "__main__":
